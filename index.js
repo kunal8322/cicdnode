e3623b795c8f7af246f014ec8cad470462a650b1
Kunal Shejwal <ep.kunalshejwal@gmail.com>
2024-08-13 12:51:13 +0530

    function add

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log("This is just a test1");
 console.log("This is just a test2");
 
 console.log(new Date());
+
+
+const datafunction=()=>{
+    console.log("this is function call");
+}
+datafunction()
